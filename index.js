fafbe800811cfa4d7e516e204a365d66cd8f8143
mag <mag.it.use@gmail.com>
2018-08-03 19:08:59 +0000

    exercise for JSON Package

---
+++ index.js
@@ -0,0 +1,19 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World");
+console.log(message);
+
+var colorRed = chalk.red("Red is HERE!");
+console.log(colorRed);
+
+var colorBlue = chalk.blue("Blue is HERE!");
+console.log(colorBlue);
+
+var colorBlack = chalk.black("Black is HERE!");
+console.log(colorBlack);
+
+var colorGreen = chalk.green("Green is HERE!");
+console.log(colorGreen);
+
+var colorWhite = chalk.white("White is HERE!");
+console.log(colorWhite);
